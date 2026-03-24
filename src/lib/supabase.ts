fdf65e018e7e128e0357568001a19dee0f9ddb6f
exi66 <eternityexi@gmail.com>
2026-03-24 17:55:45 +0400

    chore(supabase): change url for get by id

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,9 +3,9 @@ import { PostgrestClient } from "@supabase/postgrest-js"
 export const apiUrl = import.meta.env.VITE_API_URL
 
 export const getUrlToCommentById = (id: number) =>
-  new URL(`comments?select=*&id=eq.${id}`, apiUrl).toString()
+  new URL(`comments?id=eq.${id}`, apiUrl).toString()
 
 export const getUrlToPostById = (id: number) =>
-  new URL(`posts?select=*&id=eq.${id}`, apiUrl).toString()
+  new URL(`posts?id=eq.${id}`, apiUrl).toString()
 
 export const supabase = new PostgrestClient(apiUrl)
